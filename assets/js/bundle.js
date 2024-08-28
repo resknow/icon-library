98a678e772e59a973d21e8939d758bc18eca5aab
Chris Galbraith <chris@resknow.co.uk>
2024-08-28 14:29:38 +0100

    Add click to copy

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -2,3 +2,16 @@ import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 Alpine.start();
+
+window.copyToClipboard = function(value) {
+    const el = document.createElement('textarea');
+    el.value = value;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+}
+
+window.copyInnerHTMLToClipboard = function(element) {
+    copyToClipboard(element.innerHTML.trim());
+}
